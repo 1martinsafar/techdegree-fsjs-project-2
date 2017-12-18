3b810f27f54cb565388c066d8a5d5f8907f8184c
Martin Safar <1martinsafar@gmail.com>
2017-12-18 18:35:44 +0100

    Cleaned code, removed debugging code

--- scripts.js
+++ scripts.js
@@ -1,34 +1,20 @@
-'use strict';
+"use strict";
 
-// an array of students
+// An array of students
 const students = document.getElementsByClassName('student-item');
 const page = document.querySelector('.page');
 
+// Displays the appropriate 10 students for the current page
 function displayPage(pageNumber, studentsList) {
-  console.log(">>>>> displayPage RUNNING <<<<<");
   // Hiding all students on the page
-  for (let i = 0; i < studentsList.length; i++) {
-    studentsList[i].style.display = "none";
-  }
-  // New Hiding all students on the page
   const allStudents = document.querySelectorAll('.student-item');
   for (let i = 0; i < allStudents.length; i++) {
     allStudents[i].style.display = "none";
   }
-  // if student should be on this page number then show the student
+  // Shows the student if he should be on the current page number
   for (let i = 0; i < studentsList.length; i++) {
-    // 1 => 0-9
-    // 2 => 10-19
     let studentPage = Math.floor((i / 10) + 1);
-    console.log("i: " + i);
-    console.log("studentPage " + studentPage);
-    // console.log(typeof studentPage);
-    console.log("pageNumber " + pageNumber);
-    // console.log(typeof pageNumber);
-    console.log("studentPage === pageNumber");
-    console.log(studentPage === pageNumber);
     if (studentPage === pageNumber) {
-      console.log("chosen i: " + i);
       studentsList[i].style.display = "block";
     }
   }
@@ -36,18 +22,13 @@ function displayPage(pageNumber, studentsList) {
 
 // Creates and displays the pagination links
 function displayLinks(list) {
-  console.log(">>>>> displayLinks RUNNING <<<<<");
-  console.log(list);
-  console.log(list.length);
-  // Calculate how many pages are needed
+  // Calculates how many pages are needed
   let length = list.length;
   let pageNumbers = 0;
   while (length > 0) {
-    pageNumbers++;
+    pageNumbers += 1;
     length -= 10;
   }
-  console.log("PAGES: " + pageNumbers);
-
   // Creating the link section
   let pages = '';
   for (let i = 0; i < pageNumbers; i++) {
@@ -71,16 +52,11 @@ function displayLinks(list) {
   defaultPage.className = "active";
   // When you click a link, the appropriate students display
   paginationDiv.addEventListener("click", function(e) {
-    console.log(">>>>> PAGINATION EVENT CLICKED LINK <<<<<");
     let linksList = document.querySelectorAll(".pagination a");
     let link = e.target;
     let number = link.textContent;
     // Converting the string number to an integer
     number = parseInt(number);
-    // console.log("CLICKED: ");
-    // console.log("LINK TEXT CONTENT " + link.textContent);
-    // console.log("NUMBER: " + number);
-
     // Removing the display from the previous link
     for (let i = 0; i < linksList.length; i++) {
       linksList[i].className = "";
@@ -88,7 +64,6 @@ function displayLinks(list) {
     // Giving the current page the active link class
     if (link.tagName === 'A') {
       link.className = "active";
-      console.log();
       displayPage(number, list);
     }
   });
@@ -99,10 +74,7 @@ function displayLinks(list) {
   }
   // Adding search functionality
   const searchButton = document.querySelector(".student-search button");
-  console.log(searchButton);
-  // searchButton.addEventListener("click", searchStudents);
   searchButton.addEventListener("click", function() {
-    console.log(">>>>> SEARCH BUTTON EVENT CLICKED <<<<<");
     const paginationDiv = document.querySelector('.pagination');
     if (paginationDiv) {
       page.removeChild(paginationDiv);
@@ -111,8 +83,8 @@ function displayLinks(list) {
   });
 }
 
+// Creates the search field
 function createSearchComponent() {
-  console.log(">>>>> createSearchComponent RUNNING <<<<<");
   const pageHeader = document.querySelector('.page-header');
   const searchDiv = document.createElement('div');
   const searchComponent = `
@@ -124,42 +96,31 @@ function createSearchComponent() {
   pageHeader.appendChild(searchDiv);
 }
 
+// Let's the user search for students, displaying the ones that match
 function searchStudents() {
-  console.log(">>>>> searchStudents RUNNING <<<<<");
   let inputValue = document.querySelector(".student-search input").value;
-  console.log(inputValue);
   let filter = inputValue.toUpperCase();
   // Loop over the student list, and for each student…
   const names = document.querySelectorAll('.student-details h3');
   const emails = document.querySelectorAll('.student-details .email');
   let matchedStudents = [];
   for (let i = 0; i < students.length; i++) {
-    // ...obtain the student’s name…
-    // ...and the student’s email…
+    // Search is based on the student's name and email
     let studentName = names[i].textContent;
     let studentEmail = emails[i].textContent;
-    // console.log(studentName);
-    // console.log(studentEmail);
 
-    // ...if the search value is found inside either email or name…
+    // If the search value is found inside either email or name
+    // We add this student to list of “matched” students;
     let foundName = (studentName.toUpperCase().indexOf(filter) > -1);
     let foundEmail = (studentEmail.toUpperCase().indexOf(filter) > -1);
     if (foundName || foundEmail) {
-      // ...add this student to list of “matched” student;
       matchedStudents.push(students[i]);
     }
   }
-  console.log('MATCHED STUDENTS: ' + matchedStudents.length);
-  console.log(matchedStudents);
   let matchedLength = matchedStudents.length;
-  // If there’s no “matched” students…
+  // If there’s no “matched” students, a message is displayed
   let exists_notFound = document.querySelector('h1');
-  console.log("EXISTS notfound?");
-  console.log(exists_notFound);
   if (!exists_notFound) {
-    console.log(">>>>> CREATING notFound  <<<<<");
-    console.log(">>>>> CREATING notFound  <<<<<");
-    console.log(">>>>> CREATING notFound  <<<<<");
     let notFound = document.createElement('h1');
     notFound.innerHTML = 'No students found!';
     notFound.className = 'not-found';
@@ -188,18 +149,17 @@ function searchStudents() {
   displayPage(1, matchedStudents);
 }
 
-
 // Running code //
 //////////////////
 
+// Showing maximum of 10 students at the beginning, hiding others
 if (students.length > 10) {
   for (let i = 10; i < students.length; i++) {
-    // console.log(students[i]);
     students[i].style.display = "none";
   }
+  // Adding the pagination of there are more than 10 students
+  displayLinks(students);
 }
-displayLinks(students);
-
 
 
 
